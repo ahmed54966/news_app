d7e67672207403ffde08f0b338ce6cb247324068
Ahmed Mahmoud <am4402211@gmail.com>
2026-01-26 16:40:57 +0200

    Added Easy Localization Pachage

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
-
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:news/home/home_screen.dart';
 
-
-
-
-void main(){
-
-const MyApp();
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale("ar")],
+      path: 'assets/translations', // <-- change the path of the translation files 
+      fallbackLocale: const Locale('en'), 
+      startLocale: const Locale('en'), 
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,10 +23,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
 
-
+///easy_localization
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+///routes
     initialRoute: '/homeScreen',
   routes: {
     // When navigating to the "/" route, build the FirstScreen widget.
