e7fdbc0eb25c6518ee0bc77b6e046b1adb31a684
Ahmed Mahmoud <am4402211@gmail.com>
2026-02-01 19:40:50 +0200

    Ended of Settings Tab

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
       supportedLocales: [Locale('en'), Locale("ar")],
       path: 'assets/translations', // <-- change the path of the translation files 
       fallbackLocale: const Locale('en'), 
-      startLocale: const Locale('ar'), 
+      startLocale: const Locale('en'), 
       child: MyApp()
     ),
   );
